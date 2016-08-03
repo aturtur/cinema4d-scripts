58a569a8fb0cc55bec8dfda8d272f5fb35a727f7
aturtur <rautio.arttu@gmail.com>
2016-08-03 22:23:20 +0300

    new script

---
+++ mml-asc-to-point-cloud.py
@@ -0,0 +1,40 @@
+# MML (maanmittauslaitos) [NLS (national land survey of finland)]
+# https://tiedostopalvelu.maanmittauslaitos.fi/tp/kartta?lang=fi
+# elevation model - asc file to point cloud
+import c4d
+
+def main():
+    
+    # file location on computer
+    fopen = open("C:\L4133B.asc")
+
+    points = []
+    lineNb = 0
+    rowNb = 0.0
+    size = 3000
+
+    for line in fopen:
+        if lineNb > 6:
+            ptnum = 0.0
+            posz = rowNb / float(size -1) * size
+            line = line[1:]
+
+            for i in line.split(" "):
+                posx = ptnum / float(size - 1) * size
+                posy = float(i)
+                pos = c4d.Vector(posx, posy, posz)
+                c4d.StatusSetBar((lineNb/30))
+                points.append(pos)
+                ptnum += 1 
+
+            rowNb += 1
+        lineNb += 1
+    
+    poly = c4d.PolygonObject(len(points),0)
+    poly.SetAllPoints(points)    
+    doc.InsertObject(poly)
+    c4d.StatusClear()
+    fopen.close
+
+if __name__=='__main__':
+    main()

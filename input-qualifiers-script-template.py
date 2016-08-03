ad854d4c0e80669b0b2d785838c343146551dd89
aturtur <rautio.arttu@gmail.com>
2016-08-04 00:07:07 +0300

    new script

---
+++ input-qualifiers-script-template.py
@@ -0,0 +1,32 @@
+# if holding input qualifier at the time when the script is excuted
+import c4d
+from c4d import gui
+
+def main():
+    key = False
+    
+    bc = c4d.BaseContainer()
+    if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+        if bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QCTRL:
+            print "Control pressed"
+            # code here
+            c4d.EventAdd()
+            return
+        
+    if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+        if bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QALT:
+            print "Alt pressed"
+            # code here
+            c4d.EventAdd()
+            return
+
+    if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+        if bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QSHIFT:
+            print "Shift pressed"
+            # code here
+            c4d.EventAdd()
+            return
+    
+
+if __name__=='__main__':
+    main()

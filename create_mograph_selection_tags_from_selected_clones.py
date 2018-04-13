a47da5d0c631ff68d1a412c614cdf37de885f652
Arttu Rautio <rautio.arttu@gmail.com>
2018-04-13 09:50:27 +0300

    improved

--- create_mograph_selection_tags_from_selected_clones.py
+++ create_mograph_selection_tags_from_selected_clones.py
@@ -4,25 +4,29 @@ from c4d.modules import mograph as mo
 def main():
     doc.StartUndo()    
     tag = op.GetLastTag()
+    tags = op.GetTags()
     md = mo.GeGetMoData(op)
     cnt = md.GetCount()    
     selection = mo.GeGetMoDataSelection(tag)
-    x = 0
+    prefix = "ms"
+    sep = "_"
+    x = 0    
+    for k in reversed(tags):
+        if k.GetName().split("_")[0] == prefix:
+            x = x+1
     for i in reversed(xrange(0,cnt)):
         if selection.IsSelected(i) == True:
             t = c4d.BaseTag(1021338)
-            t[c4d.ID_BASELIST_NAME] = "ms_"+str(x)
+            t[c4d.ID_BASELIST_NAME] = prefix+sep+str(x)
             s = c4d.BaseSelect()
             s.Select(i)
             op.InsertTag(t)
             doc.AddUndo(c4d.UNDOTYPE_NEW, t)
             mo.GeSetMoDataSelection(t, s)
-
-            x = x + 1
-            doc.AddUndo(c4d.UNDOTYPE_DELETE, tag)
+            x = x + 1            
     tag.Remove()
+    doc.AddUndo(c4d.UNDOTYPE_DELETE, tag)
     doc.EndUndo()
-    c4d.EventAdd()
-    
+    c4d.EventAdd()    
 if __name__=='__main__':
     main()

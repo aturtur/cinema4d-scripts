47e66228dadbd2bf24b74cb719749a43d6a6d62e
Arttu Rautio <rautio.arttu@gmail.com>
2018-05-31 15:06:38 +0300

    update

--- print-what-generators-uses-this-effector.py
+++ print-what-generators-uses-this-effector.py
@@ -1,5 +1,7 @@
 import c4d
 
+select = False
+
 def GetNextObject(op):
     if op==None:
         return None
@@ -26,6 +28,9 @@ def IterateHierarchy(op, obj):
                 for i in range(0, effectors.GetObjectCount()):
                     if obj.GetGUID() == effectors.ObjectFromIndex(doc,i).GetGUID():
                         print op.GetName()
+                        if select:
+                            doc.AddUndo(c4d.UNDOTYPE_BITS, op)
+                            op.SetBit(c4d.BIT_ACTIVE)
             else: # MoText
                 effectorsall = op[c4d.MGTEXTOBJECT_EFFECTORLIST_ALL]
                 effectorsline = op[c4d.MGTEXTOBJECT_EFFECTORLIST_LINE]
@@ -34,24 +39,42 @@ def IterateHierarchy(op, obj):
                 for i in range(0, effectorsall.GetObjectCount()):
                     if obj.GetGUID() == effectorsall.ObjectFromIndex(doc,i).GetGUID():
                         print op.GetName()+" (All)"
+                        if select:
+                            doc.AddUndo(c4d.UNDOTYPE_BITS, op)
+                            op.SetBit(c4d.BIT_ACTIVE)
                 for i in range(0, effectorsline.GetObjectCount()):
                     if obj.GetGUID() == effectorsline.ObjectFromIndex(doc,i).GetGUID():
                         print op.GetName()+" (Lines)"
+                        if select:
+                            doc.AddUndo(c4d.UNDOTYPE_BITS, op)
+                            op.SetBit(c4d.BIT_ACTIVE)
                 for i in range(0, effectorsword.GetObjectCount()):
                     if obj.GetGUID() == effectorsword.ObjectFromIndex(doc,i).GetGUID():
                         print op.GetName()+" (Words)"
+                        if select:
+                            doc.AddUndo(c4d.UNDOTYPE_BITS, op)
+                            op.SetBit(c4d.BIT_ACTIVE)
                 for i in range(0, effectorschar.GetObjectCount()):
                     if obj.GetGUID() == effectorschar.ObjectFromIndex(doc,i).GetGUID():
                         print op.GetName()+" (Letters)"
+                        if select:
+                            doc.AddUndo(c4d.UNDOTYPE_BITS, op)
+                            op.SetBit(c4d.BIT_ACTIVE)
+                        
         op = GetNextObject(op)
     return None
 
 def main():
+    doc.StartUndo()
     start_object = doc.GetFirstObject()
     obj = doc.GetActiveObject()
     if obj == None: return None
     IterateHierarchy(start_object, obj)
+    if select:
+        doc.AddUndo(c4d.UNDOTYPE_BITS, obj)
+        obj.DelBit(c4d.BIT_ACTIVE)
     c4d.EventAdd()
+    doc.EndUndo()    
 
 if __name__=='__main__':
     main()

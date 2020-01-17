e6c2855ac017818baf937b2256d9bcd82135e7c8
aturtur <rautio.arttu@gmail.com>
2020-01-17 16:48:37 +0200

    Update

--- AR_CreateControlNullsForJoints.py
+++ AR_CreateControlNullsForJoints.py
@@ -1,11 +1,11 @@
 """
-AR_CreateStickyNullsFromObjects
+AR_CreateControlNullsForJoints
 
 Author: Arttu Rautio (aturtur)
 Website: http://aturtur.com/
-Name-US: AR_CreateStickyNullsFromObjects
-Description-US: Create null objects with constraint tag (PSR) from selected object(s)
-Written for Maxon Cinema 4D R20.057
+Name-US: AR_CreateControlNullsForJoints
+Description-US: Create control null object(s) from selected joint(s)
+Written for Maxon Cinema 4D R21.057
 """
 # Libraries
 import c4d
@@ -49,7 +49,7 @@ def SetGlobalScale(obj, scale): # Set object's global scale
     m.v3 = m.v3.GetNormalized() * scale.z
     obj.SetMg(m)
 
-def CreateNulls(obj):
+def CreateNulls(obj, nullList):
     doc = c4d.documents.GetActiveDocument() # Get active Cinema 4D document
     try: # Try to execute following script
         objectNull = c4d.BaseObject(c4d.Onull) # Initialize null object
@@ -61,20 +61,38 @@ def CreateNulls(obj):
         doc.InsertObject(objectNull) # Insert null to document
         objectNull.SetName(obj.GetName()+"_Null") # Set null's name
         objectNull[c4d.NULLOBJECT_DISPLAY] = 2 # Set null's display mode circle
-        objectNull.InsertTag(c4d.BaseTag(1019364)) # Insert constraint tag to null
-        tag = objectNull.GetFirstTag() # Select constraint tag
-        tag[c4d.ID_CA_CONSTRAINT_TAG_PSR] = 1 # Activate PSR constraint
-        tag[10001] = obj # Set Target        
+        nullList.append(objectNull)
+
         doc.AddUndo(c4d.UNDOTYPE_NEW, objectNull) # Add undo command for adding new object
     except: # If something went wrong
         pass # Do nothing
+
+def Order(nullList):
+    for i in range(0, len(nullList)):
+        if i != 0:
+            nullList[i].InsertUnder(nullList[i-1])
+            nullList[i].SetMg(~nullList[i-1].GetMg()*nullList[i].GetMg())
+            
+def ResetPSR(obj):
+    obj.SetRelPos(c4d.Vector(0,0,0)) # Reset relative position
+    obj.SetRelRot(c4d.Vector(0,0,0)) # Reset relative rotation
+    obj.SetRelScale(c4d.Vector(1,1,1)) # Reset relative scale
 
 def main():
     doc = c4d.documents.GetActiveDocument() # Get active Cinema 4D document
     doc.StartUndo() # Start recording undos
     selection = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN) # Get active selection
+    nullList = []
     for obj in selection: # Loop through selected items
-        CreateNulls(obj) # Run CreateNulls function
+        CreateNulls(obj, nullList) # Run CreateNulls function
+    Order(nullList)
+    for i, obj in enumerate(selection): # Loop through selected items, again
+        tag = c4d.BaseTag(1019364) # Init constraint tag
+        obj.InsertTag(tag) # Insert tag to joint
+        tag[c4d.ID_CA_CONSTRAINT_TAG_PARENT] = 1 # Activate parent constraint
+        tag[30001] = nullList[i] # Set Target
+        ResetPSR(obj) # Rest PSR
+
     doc.EndUndo() # Stop recording undos
     c4d.EventAdd() # Refresh Cinema 4D
 
